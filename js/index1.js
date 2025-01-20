8489c639916b66e61faa5cab546274d693a6335a
AICNN1 <132210726+AICNN1@users.noreply.github.com>
2025-01-20 22:47:45 +0800

    Update index1.js

--- js/index1.js
+++ js/index1.js
@@ -103,10 +103,10 @@
 			element.className = 'element';
 			element.style.backgroundColor = 'rgba(0,127,127,' + ( Math.random() * 0.5 + 0.25 ) + ')';
 
-			element.style.backgroundImage = 'url("images/photo'+ (i/5 + 1) +'.JPG")';
+			element.style.backgroundImage = 'url("images/photo'+ (i/5 + 1) +'.jpg")';
 			// element.style.backgroundSize = 'cover';
-			element.title = '翠蕊滴3D相册❤ - '+ (i/5 + 1) +'/'+ (table.length/5);
-			element.setAttribute('href', 'images/photo'+ (i/5 + 1) +'.JPG');
+			element.title = '洋姐滴3D相册❤ - '+ (i/5 + 1) +'/'+ (table.length/5);
+			element.setAttribute('href', 'images/photo'+ (i/5 + 1) +'.jpg');
 			
 			// var number = document.createElement( 'div' );
 			// number.className = 'number';
